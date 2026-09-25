e2b4d4490c049bc5a1da798f67cac353efa17aff
Nitiema Allassane <allassanenitiema@gmail.com>
2026-09-25 08:37:49 +0000

    (add): Ajout de la resource tasks pour le CRUD des taches

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { UsersModule } from './users/users.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { TasksModule } from './tasks/tasks.module.js';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
